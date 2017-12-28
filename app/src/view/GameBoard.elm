3e406029b386b5811de5fea0f852404d4f850d6e
Andrej <andrej.hafn3r@gmail.com>
2017-12-28 16:24:12 +0100

    Added picture assets

--- app/src/view/GameBoard.elm
+++ app/src/view/GameBoard.elm
@@ -1,21 +1,30 @@
 module GameBoard exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (src, style)
+import Html.Events exposing (onClick)
 import List
 import Model exposing (Model, Msg)
 import Styles exposing (squareStyle)
+import Svg.Attributes exposing (visibility)
 
 
 view : Model -> Html Msg
 view model =
-    div [] (List.map (renderRow model) (List.range 0 7))
+    div [] (renderBoard model)
 
 
 
--- renderBoard : Model -> Html Msg
--- renderBoard model =
---     List.map (\a -> a) [0..2]
+-- Returns a list of Html elements that represnt a chessboard
+
+
+renderBoard : Model -> List (Html Msg)
+renderBoard model =
+    List.map (renderRow model) (List.range 0 7)
+
+
+
+-- Creates a row for a board
 
 
 renderRow : Model -> Int -> Html Msg
@@ -23,6 +32,38 @@ renderRow model row =
     div [] (List.map (renderSquare model row) (List.range 0 7))
 
 
+
+-- Renders the content of a certain square in a board and send events when clicked
+
+
 renderSquare : Model -> Int -> Int -> Html Msg
 renderSquare model row col =
-    div [ squareStyle ] [ text (toString row ++ " " ++ toString col) ]
+    let
+        selectedField =
+            case model.selectedSquare of
+                ( a, b ) ->
+                    if a == row && b == col then
+                        True
+                    else
+                        False
+    in
+    div
+        [ squareStyle selectedField
+        , onClick (Model.SquareSelected row col)
+        ]
+        [ img
+            [ src "assets/king_black.svg"
+            , style
+                [ ( "visibility"
+                  , if selectedField then
+                        "visible"
+                    else
+                        "hidden"
+                  )
+                , ( "height", "20px" )
+                , ( "width", "20px" )
+                ]
+            ]
+            []
+        , text (toString row ++ " " ++ toString col)
+        ]
