669e5852c1a445acc64e81abbccaca8377614617
Andrej <andrej.hafn3r@gmail.com>
2017-12-28 13:36:16 +0100

    Working app

--- app/src/view/View.elm
+++ app/src/view/View.elm
@@ -1,11 +1,13 @@
 module View exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Model exposing (Model, Msg)
 
 
--- View part for the Chess game
+-- Main view of the app, we add subcomponents to separate files and expose their view functions inside this one
 
 
 view : Model -> Html Msg
 view model =
-    div [] []
+    div [] [ input [] [] ]
