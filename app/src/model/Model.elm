5a6945e5a47a6215e833a1d809869e8b19a0d9b5
Anze Mur <anze.mur50@gmail.com>
2018-01-18 19:43:53 +0100

    Game over menu

--- app/src/model/Model.elm
+++ app/src/model/Model.elm
@@ -21,6 +21,7 @@ type Route
     | GameTypeMenu
     | GameOne
     | GameTwo
+    | GameOverMenu
 
 
 type Msg
@@ -32,6 +33,7 @@ type Msg
     | ShowMainMenu
     | ShowGameTypesMenu
     | ShowHighscoresMenu
+    | ShowGameOverMenu
     | Tick Time
 
 
